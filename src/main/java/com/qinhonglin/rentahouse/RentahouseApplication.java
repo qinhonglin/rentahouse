cdc1cb7b78ba0067039b9714efd5300196803640
qinhonglin <1072099683@qq.com>
2018-04-17 20:12:19 +0800

    Initial commit

---
+++ src/main/java/com/qinhonglin/rentahouse/RentahouseApplication.java
@@ -0,0 +1,12 @@
+package com.qinhonglin.rentahouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentahouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentahouseApplication.class, args);
+	}
+}
---
+++ src/test/java/com/qinhonglin/rentahouse/RentahouseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qinhonglin.rentahouse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RentahouseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
